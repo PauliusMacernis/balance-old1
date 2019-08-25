b176e5f4ae89014e2ba65521729cda5cd4df099d
Paulius Macernis <paulius@emotion.lt>
2019-08-25 14:09:10 +0300

    Project description and url modified.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject balance "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Website for balance tracking"
+  :url "http://localhost:3000"
 
   :dependencies [[ch.qos.logback/logback-classic "1.2.3"]
                  [cheshire "5.8.1"]
